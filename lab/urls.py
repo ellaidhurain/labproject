1d0533c947ec312504a4e880fa40820f64043c5e
Satyam-79 <satyamverma79@gmail.com>
2023-07-11 15:36:20 +0530

    updated customer_verification url

--- lab/urls.py
+++ lab/urls.py
@@ -29,5 +29,9 @@ urlpatterns = [
         update_form_status,
         name="update_form_status",
     ),
-    path("customer_verification/<int:lab_id>/<int:supplier_id>", customer_verification, name="customer_verification"),
+    path(
+        "customer_verification/<int:id>",
+        customer_verification,
+        name="customer_verification",
+    ),
 ]
