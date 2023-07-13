098a2c6bd247ba3d08025b96f0ae99d644e48de8
Satyam-79 <satyamverma79@gmail.com>
2023-07-13 23:33:42 +0530

    fix username bug

--- lab/serializers.py
+++ lab/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 # from django.contrib.auth.models import User
 from .models import User, Supplier, SupplierForm, Laboratory, Customer
 from django.core.exceptions import ValidationError
@@ -8,29 +9,30 @@ from django.conf import settings
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'user_type', 'password']
+        fields = ["id", "username", "user_type", "password"]
         extra_kwargs = {
-            'password': {'write_only': True},
+            "password": {"write_only": True},
         }
 
     def create(self, validated_data):
-        user_type = validated_data['user_type']
+        user_type = validated_data["user_type"]
         user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            user_type=user_type
+            username=validated_data["username"],
+            password=validated_data["password"],
+            user_type=user_type,
         )
 
-        if user_type == 'S':  # Supplier user
+        if user_type == "S":  # Supplier user
             supplier = Supplier.objects.create(user=user)
-            
-        if user_type == 'C':  # Supplier user
+
+        if user_type == "C":  # Supplier user
             supplier = Customer.objects.create(user=user)
-        
-        if user_type == 'L':  # Supplier user
+
+        if user_type == "L":  # Supplier user
             supplier = Laboratory.objects.create(user=user)
-            
+
         return user
+
 
 class SupplierFormSerializer(serializers.ModelSerializer):
     sub_proof_url = serializers.SerializerMethodField()
@@ -41,47 +43,47 @@ class SupplierFormSerializer(serializers.ModelSerializer):
     customer = serializers.SerializerMethodField()
     laboratory = serializers.SerializerMethodField()
     supplier = serializers.SerializerMethodField()
-   
+
     class Meta:
         model = SupplierForm
-        fields = '__all__'
-        
+        fields = "__all__"
+
     def get_sub_proof_url(self, obj):
         if obj.sub_proof:
             return self._get_full_url(obj.sub_proof.url)
         return None
-        
+
     def get_product_image_url(self, obj):
         if obj.product_image:
             return self._get_full_url(obj.product_image.url)
         return None
-        
+
     def get_certificate_image_url(self, obj):
         if obj.certificate_image:
             return self._get_full_url(obj.certificate_image.url)
         return None
-    
+
     def get_accreditation_image_url(self, obj):
         if obj.certificate_image:
             return self._get_full_url(obj.accreditation_image.url)
         return None
-        
+
     def get_test_result_image_url(self, obj):
         if obj.test_result_image:
             return self._get_full_url(obj.test_result_image.url)
         return None
-    
+
     def _get_full_url(self, relative_url):
-        if not relative_url.startswith('http'):
+        if not relative_url.startswith("http"):
             return f"{settings.BASE_URL.rstrip('/')}{relative_url}"
         return relative_url
-    
+
     def get_customer(self, obj):
         customer = obj.customer
         if customer:
             return {
                 "id": customer.id,
-                "username": customer.username,
+                "username": customer.user.username,
             }
         return None
 
@@ -90,7 +92,7 @@ class SupplierFormSerializer(serializers.ModelSerializer):
         if laboratory:
             return {
                 "id": laboratory.id,
-                "username": laboratory.username,
+                "username": laboratory.user.username,
             }
         return None
 
@@ -99,24 +101,24 @@ class SupplierFormSerializer(serializers.ModelSerializer):
         if supplier:
             return {
                 "id": supplier.id,
-                "username": supplier.username,
+                "username": supplier.user.username,
             }
         return None
-        
+
+
 class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
-        fields = '__all__'
+        fields = "__all__"
 
 
 class LaboratoryFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Laboratory
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = '__all__'
-        
+        fields = "__all__"
